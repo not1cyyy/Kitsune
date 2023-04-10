e64fbe7ea3ee0224f2160ccf1a0a563f9cddb81b
not1cyyy <firas1120@gmail.com>
2023-04-10 23:20:19 +0100

    Added replicas folder

--- replicate.cs
+++ replicate.cs
@@ -11,12 +11,29 @@ namespace SelfReplicatingCode
         static void Main(string[] args)
         {
             string fileName = Assembly.GetExecutingAssembly().Location;
+            string directoryPath = Path.Combine(Path.GetDirectoryName(fileName), "generated-files");
+
+            // create the directory if it doesn't exist
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
             for (int i = 0; i < MAX_ITERATIONS; i++)
             {
-                string newFileName = Path.GetDirectoryName(fileName) + "\\" +
-                    Path.GetFileNameWithoutExtension(fileName) + i + Path.GetExtension(fileName);
+                string newFileName = Path.Combine(directoryPath,
+                    Path.GetFileNameWithoutExtension(fileName) + i + Path.GetExtension(fileName));
                 File.Copy(fileName, newFileName, true);
             }
+            PlayRickRoll(); // Insert in loop if you want to rickroll everytime you replicate (spam)
+        }
+
+        static void PlayRickRoll()
+        {
+            string url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+            // Launch the default web browser to play the video
+            System.Diagnostics.Process.Start(url);
         }
     }
 }
